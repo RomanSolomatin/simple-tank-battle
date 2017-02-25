4e3676cb9c7a150770379efd07e344e0995efb99
TDOAN1H\Thanh <doant@outlook.com>
2017-02-24 22:24:08 -0800

    Make Tank and Tank Movement class loose coupling.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,8 +2,17 @@
 
 #include "BattleTank.h"
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f "), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
 }
